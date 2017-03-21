243d2e52c3270cae08b5c56df24eab5d5e5c857d
Archanciel <jp.schnyder@gmail.com>
2017-03-21 20:36:14 +0100

    add print and plot code

--- partTwo.py
+++ partTwo.py
@@ -9,6 +9,8 @@
 
 ## ----------------------- Part 1 ---------------------------- ##
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 # X = (hours sleeping, hours studying), y = Score on test
 X = np.array(([3,5], [5,1], [10,2]), dtype=float)
@@ -19,10 +21,12 @@ X = X/np.amax(X, axis=0)
 y = y/100 #Max test score is 100
 
 ## ----------------------- Part 2 ---------------------------- ##
+verbose=True
+doPlot = True
 
 class Neural_Network(object):
     def __init__(self):        
-        #Define Hyperparameters
+        #Define Hyperparameters == col number
         self.inputLayerSize = 2
         self.outputLayerSize = 1
         self.hiddenLayerSize = 3
@@ -34,11 +38,63 @@ class Neural_Network(object):
     def forward(self, X):
         #Propagate inputs though network
         self.z2 = np.dot(X, self.W1)
+
+        if verbose:
+            print("X")
+            print(X)
+            print("W1")
+            print(self.W1)
+            title = "z2 = X . W1"
+            print("%s" % title)
+            print(self.z2)
+            print()
+            if doPlot:
+                plt.title(title)
+                plt.plot(self.z2)
+                plt.show()
+
         self.a2 = self.sigmoid(self.z2)
+
+        if verbose:
+            title = "a2 = sigmoid(z2)"
+            print(title)
+            print(self.a2)
+            print()
+            if doPlot:
+                plt.title(title)
+                plt.plot(self.a2)
+                plt.show()
+
         self.z3 = np.dot(self.a2, self.W2)
-        yHat = self.sigmoid(self.z3) 
+
+        if verbose:
+            print("W2")
+            print(self.W2)
+            title = "z3 = a2 . W2"
+            print(title)
+            print(self.z3)
+            print()
+            if doPlot:
+                plt.title(title)
+                plt.plot(self.z3)
+                plt.show()
+
+        yHat = self.sigmoid(self.z3)
+
+        if verbose:
+            title = "yHat = sigmoid(z3)"
+            print(title)
+            print(yHat)
+            print()
+            if doPlot:
+                plt.title(title)
+                plt.plot(yHat)
+                plt.show()
+
         return yHat
         
     def sigmoid(self, z):
         #Apply sigmoid activation function to scalar, vector, or matrix
         return 1/(1+np.exp(-z))
+nn = Neural_Network()
+nn.forward(X)
